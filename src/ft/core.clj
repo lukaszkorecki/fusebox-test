670d182d73714b0db1d223458d12375f9b61c4b4
Łukasz Korecki <lukasz@korecki.me>
2024-11-16 18:13:57 +0100

    Final clean up (1) (final)

--- src/ft/core.clj
+++ src/ft/core.clj
@@ -5,39 +5,41 @@
   "Fake HTTP request fn, depending on the input
   it will return a HTTP response map, and given count `n`
   it can simulate the request working after number of retries"
-  [{:keys [resp n exc-class]
+  [{:keys [resp n exc]
     :or {n 1}
     :as args}]
-  #_(tap> {:request resp :n n})
   (cond
+    ;; simple cases
     (= resp :ok)
     {:status 200 :body {:msg "ok"}}
 
     (= resp :not-found)
     {:status 404 :body {:msg "gone"}}
 
+    ;; simulate a request that always fails
+    (= resp :srv-hard-err)
+    {:status 501 :body {:msg "oh dear"}}
+
     ;; simulate failing request...
-    (and (= resp :srv-errr) (< n 2))
+    (and (= resp :srv-err) (< n 2))
     {:status 500 :body {:msg "oh no"}}
     ;; and make it work on 3rd try
-    (and (= resp :srv-errr) (= n 2))
+    (and (= resp :srv-err) (>= n 2))
     {:status 200 :body {:msg "finally working"}}
 
+    ;; simulate http client throwing
     (= resp :exc)
-    (throw exc-class)
+    (throw exc)
 
     :else
     (throw (ex-info "unknown response type" args))))
 
 (def retry-opts
   (retry/init {::retry/retry? (fn [n _duration-ms _exc]
-                                #_(tap> {:n n})
                                 (<= n 5))
 
-               ;; this is not called at all
-               ::retry/success? (fn [{:keys [status] :as res}]
-                                  (tap> {::res res}) ;; why is this nil?
-                                  (not (= status 500)))
+               ::retry/success? (fn [{:keys [status] :as _res}]
+                                  (not (>= status 500)))
 
                ::retry/delay (constantly 10)}))
 
@@ -47,8 +49,8 @@
     #_{:clj-kondo/ignore [:unresolved-symbol]}
     (retry/with-retry [retry-count _duration-ms] retry-opts
       (track-fn retry-count)
-      #_(tap> {:rc retry-count :dur _duration-ms})
       (request (assoc req :n retry-count)))
 
     (catch clojure.lang.ExceptionInfo err
-      (ex-data err))))
+      {:data (ex-data err)
+       :cause (ex-cause err)})))
--- test/ft/core_test.clj
+++ test/ft/core_test.clj
@@ -1,6 +1,8 @@
 (ns ft.core-test
   (:require [ft.core :as ft]
-            [clojure.test :refer [deftest testing is use-fixtures]]))
+            [clojure.test :refer [deftest testing is use-fixtures]]
+            [com.potetm.fusebox.retry :as-alias retry]
+            [com.potetm.fusebox :as-alias fb]))
 
 (def attempts (atom 0))
 
@@ -10,6 +12,32 @@
 (use-fixtures :each (fn [test]
                       (reset! attempts [])
                       (test)))
+
+(deftest sanity-checks-test
+  (testing "simple cases"
+    (is (= {:status 200 :body {:msg "ok"}}
+           (ft/request {:resp :ok})))
+
+    (is (= {:status 404 :body {:msg "gone"}}
+           (ft/request {:resp :not-found})))
+
+    (is (= {:status 501 :body {:msg "oh dear"}}
+           (ft/request {:resp :srv-hard-err}))))
+
+  (testing "exception"
+    (is (thrown-with-msg? clojure.lang.ExceptionInfo #"test"
+                          (ft/request {:resp :exc
+                                       :exc (ex-info "test" {})}))))
+
+  (testing "'eventually' working"
+    (is (= [{:body {:msg "oh no"} :status 500}
+            {:body {:msg "oh no"} :status 500}
+            {:body {:msg "finally working"} :status 200}
+            {:body {:msg "finally working"} :status 200}
+            {:body {:msg "finally working"} :status 200}]
+
+           (->> (range 0 5)
+                (mapv #(ft/request {:resp :srv-err :n %})))))))
 
 (deftest no-retries-just-works-test
   (is (= {:status 200 :body {:msg "ok"}}
@@ -23,17 +51,36 @@
                           :track-fn update-attempt-count})))
   (is (= 1 @attempts)))
 
+(deftest always-failing-test
+  ;; XXX: no way of getting this response map, I get only
+  ;; {:com.potetm.fusebox/error :com.potetm.fusebox.error/retries-exhausted
+  ;;  :com.potetm.fusebox/spec {}
+  ;;  :com.potetm.fusebox.retry/exec-duration-ms 63
+  ;;  :com.potetm.fusebox.retry/num-retries 5}
+  (is (= {:status 501 :body {:msg "oh dear"}}
+         (ft/req-w-retry {:req {:resp :srv-hard-err}
+                          :track-fn update-attempt-count})))
+  (is (= 6 @attempts)))
+
 (deftest error-retries-test
+  ;; Works as expected, since eventually it works
   (is (= {:status 200 :body {:msg "finally working"}}
          (ft/req-w-retry {:req {:resp :srv-err}
                           :track-fn update-attempt-count})))
-  (is (= 4 @attempts)))
+  (is (= 3 @attempts)))
 
 (deftest exc-retries-test
-  (is (= :com.potetm.fusebox.error/retries-exhausted,
-         (-> (ft/req-w-retry {:req {:resp :exc
-                                    :exc-class (java.net.UnknownHostException.)}
-                              :track-fn update-attempt-count})
-             :com.potetm.fusebox/error)))
+  (let [exc (java.net.UnknownHostException.)]
+    (is (= {:data
+            {:com.potetm.fusebox/error :com.potetm.fusebox.error/retries-exhausted
+             :com.potetm.fusebox/spec {} ;; XXX: why is this empty?
+             :com.potetm.fusebox.retry/num-retries 5}
 
-  (is (= 6 @attempts)))
+            :cause exc}
+
+           (-> (ft/req-w-retry {:req {:resp :exc
+                                      :exc exc}
+                                :track-fn update-attempt-count})
+               (update :data #(dissoc % ::retry/exec-duration-ms))))
+
+        (is (= 6 @attempts)))))
